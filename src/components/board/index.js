66ea00fb6a9eef507adbad7e95de900e3aebc7ab
Fernanda Luna <ferluna.rm@gmail.com>
2023-08-26 22:51:22 -0600

    Fix

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -18,6 +18,12 @@ const Board = ({ username }) => {
 	const [completedGame, setCompletedGame] = useState(false);
 
 	useEffect(() => {
+		if (guessedCards.length === images?.length / 2) {
+			setCompletedGame(true);
+		}
+	}, [guessedCards, images]);
+
+	useEffect(() => {
 		fetchImages().then((response) => {
 			let arrImgs = [...response.entries, ...response.entries];
 			arrImgs.sort(() => Math.random() - 0.5);
@@ -26,22 +32,22 @@ const Board = ({ username }) => {
 	}, []);
 
 	useEffect(() => {
-		if (clickedCards.length === 2) {
-			let ids = clickedCards.map((card) => card.imageId);
-			if (ids[0] === ids[1]) {
-				if (!guessedCards.includes(ids[0])) {
-					setRightGuess((prevVal) => prevVal + 1);
-					setGuessedCards((prev) => {
-						let newGuesses = [...prev, ids[0]];
-						return newGuesses;
-					});
-					if (guessedCards.length === images.length / 2) setCompletedGame(true);
+		if (!completedGame)
+			if (clickedCards.length === 2) {
+				let ids = clickedCards.map((card) => card.imageId);
+				if (ids[0] === ids[1]) {
+					if (!guessedCards.includes(ids[0])) {
+						setRightGuess((prevVal) => prevVal + 1);
+						setGuessedCards((prev) => {
+							let newGuesses = [...prev, ids[0]];
+							return newGuesses;
+						});
+					}
+				} else {
+					setWrongGuess((prevVal) => prevVal + 1);
 				}
-			} else {
-				setWrongGuess((prevVal) => prevVal + 1);
 			}
-		}
-	}, [clickedCards, guessedCards, images]);
+	}, [clickedCards, guessedCards, completedGame]);
 
 	const handleCardClick = (data) => {
 		if (clickedCards.length === 2) setClickedCards([]);
@@ -96,40 +102,38 @@ const Board = ({ username }) => {
 	};
 
 	return (
-		<>
-			<div className='board-wrapper'>
-				{completedGame ? (
-					<>
-						<h2 className='my-10 text-5xl'>
-							Congrats, {localStorage.getItem("username")}!
-						</h2>
-						<Lottie
-							animationData={winAnimation}
-							loop={true}
-						/>
-					</>
-				) : images ? (
-					<>
-						<h1>
-							Welcome,{" "}
-							<span className='bg-gradient-to-b from-nord-10 to-nord-15 bg-clip-text text-nord-7 text-transparent'>
-								{username}
-							</span>
-							!
-						</h1>
-						<div className='flex justify-center'>
-							<h2 className='mx-8'>Right Guesses: {rightGuess} </h2>
-							<h2 className='mx-8'>Wrong Guesses: {wrongGuess}</h2>
-						</div>
-						<div className='board grid-rows-20 sm:grid-rows-10 md:grid-rows-8 mt-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-10 lg:grid-rows-4'>
-							{imagesComponent()}
-						</div>
-					</>
-				) : (
-					loadingImg()
-				)}
-			</div>
-		</>
+		<div className='board-wrapper'>
+			{completedGame ? (
+				<>
+					<h2 className='my-10 text-5xl'>
+						Congrats, {localStorage.getItem("username")}!
+					</h2>
+					<Lottie
+						animationData={winAnimation}
+						loop={true}
+					/>
+				</>
+			) : images ? (
+				<>
+					<h1>
+						Welcome,{" "}
+						<span className='bg-gradient-to-b from-nord-10 to-nord-15 bg-clip-text text-nord-7 text-transparent'>
+							{username}
+						</span>
+						!
+					</h1>
+					<div className='flex justify-center'>
+						<h2 className='mx-8'>Right Guesses: {rightGuess} </h2>
+						<h2 className='mx-8'>Wrong Guesses: {wrongGuess}</h2>
+					</div>
+					<div className='board grid-rows-20 sm:grid-rows-10 md:grid-rows-8 mt-4 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-10 lg:grid-rows-4'>
+						{imagesComponent()}
+					</div>
+				</>
+			) : (
+				loadingImg()
+			)}
+		</div>
 	);
 };
 
